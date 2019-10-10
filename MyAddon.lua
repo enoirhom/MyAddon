c6df0248e387e60a08f1165248f78f08acd335e6
Emil <emil.noirhomme@icloud.com>
2019-10-10 13:30:59 +0200

    Clean up

--- MyAddon.lua
+++ MyAddon.lua
@@ -1,42 +1,33 @@
-
-local function getQualityNumber(bagID, slot)
-	local texture, itemCount, locked, quality, readable, lootable, itemLink = GetContainerItemInfo(bagID, slot);
-	--local itemId = GetContainerItemID(bagID, slot);
-	--DEFAULT_CHAT_FRAME:AddMessage(itemId);
-	--local itemName, itemLink, itemRarity, itemLevel, itemMinLevel, itemType, itemSubType, itemStackCount, itemEquipLoc, itemTexture, itemSellPrice = GetItemInfo(itemId);
-	return  quality;
-end
-
-local function getItemLink(bagID, slot)
-	local texture, itemCount, locked, quality, readable, lootable, itemLink = GetContainerItemInfo(bagID, slot);
-	return  itemLink;
-end
-
 local function checkBags(self, event, ...)
+	local totalAmount = 0;
+
 	for bagID = 0, 4 do
 		local nbSlot = GetContainerNumSlots(bagID);
-		
+
 		for slot = 1, nbSlot do
-			local quality = getQualityNumber(bagID, slot);
-			
-			if quality == 0 then
-				UseContainerItem(bagID, slot);
+			local texture, itemCount, locked, quality, readable, lootable, itemLink = GetContainerItemInfo(bagID, slot);
+
+			if itemLink ~= nil then
+				local itemName, iLink, itemRarity, itemLevel, itemMinLevel, itemType, itemSubType, itemStackCount, itemEquipLoc, itemTexture, itemSellPrice = GetItemInfo(itemLink);
+				if quality == 0 then
+					UseContainerItem(bagID, slot);
+					totalAmount = totalAmount + itemCount * itemSellPrice;
+				end
 			end
 		end
-		
 	end
+
+	local gold = totalAmount / 10000;
+	local roundedGold = gold - (gold % 1);
+	local silver = (gold - roundedGold) * 100;
+	local roundedSilver = silver - (silver % 1);
+	local copper = (silver - roundedSilver) * 100;
+
+	DEFAULT_CHAT_FRAME:AddMessage("Recettes: " .. roundedGold .. "g " .. roundedSilver .. "s " .. copper .. "c!");
 end
 
 DEFAULT_CHAT_FRAME:AddMessage("Hello " .. UnitName("player") .. " beau gosse");
---local link = getItemLink(0, 2);
---local itemName, itemLink, itemRarity, itemLevel, itemMinLevel, itemType, itemSubType, itemStackCount, itemEquipLoc, itemTexture, itemSellPrice = GetItemInfo(link);
---DEFAULT_CHAT_FRAME:AddMessage(itemName);
-
-local itemId = GetContainerItemID(0, 2);
-DEFAULT_CHAT_FRAME:AddMessage(itemId);
 
 local frame = CreateFrame("FRAME", "MyAddon");
 frame:RegisterEvent("MERCHANT_SHOW");
 frame:SetScript("OnEvent", checkBags);
-
-
